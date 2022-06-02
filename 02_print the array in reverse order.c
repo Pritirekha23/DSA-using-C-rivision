77469deda7ea25f0231d85ebc29e3a5cc48b987d
unknown <iampritirekha@gmail.com>
2022-06-02 12:01:32 -0700

    my second commit

---
+++ 02_print the array in reverse order.c
@@ -0,0 +1,14 @@
+//print an array in reverse order
+#include<stdio.h>
+int main()
+{
+int priti[5]={22,33,44,55,66},i;
+
+  printf("After reversing the array\n");
+  for(i=4;i>=0;i--)
+  {
+  	printf("%d\n",priti[i]);
+  }
+	return 0;
+}
+
