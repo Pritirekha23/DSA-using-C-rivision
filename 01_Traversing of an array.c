2c9f29ab5546abd1308c8e32839ce2c48821b2c0
unknown <iampritirekha@gmail.com>
2022-06-02 11:53:05 -0700

    my first commit

---
+++ 01_Traversing of an array.c
@@ -0,0 +1,27 @@
+//Traversing of an array
+#include<stdio.h>
+int main()
+{
+// int priti[10],i,size;
+//  printf("Enter the size of an arry\n");
+//  scanf("%d\n",&size);
+//  for(i=0;i<size;i++)
+//  {
+//  	 printf("Enter the %d location value\n",i);
+//     scanf("%d\n",&priti[i]);
+//  }
+//  //Traversing of array
+//  printf("Elements are\n");
+//  for(i=0;i<size;i++)
+//  {
+//  	printf("The %d location Value is %d\n",i,priti[i]);
+//  }
+  int a[4]={3,2,7,6},i;
+  printf("The elements are\n");
+  for(i=0;i<4;i++)
+    {
+ 	printf("%d\n",a[i]);
+    }
+ return 0;
+}
+
